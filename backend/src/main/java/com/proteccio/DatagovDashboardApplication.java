096c363c18961b34fc42f929b6309b751159cb5d
himwak2001 <himanshuwakodikar22@gmail.com>
2026-07-22 19:22:47 +0530

    chore: init repo

---
+++ backend/src/main/java/com/proteccio/DatagovDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.proteccio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatagovDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatagovDashboardApplication.class, args);
+	}
+
+}
---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1>Data Governance Dashboard</h1>
+      <p>Setup in progress...</p>
+    </div>
+  );
+}
+
+export default App;
